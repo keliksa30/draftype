b1170b11e4239d151a717f2d6bda02e661818158
keliksa30 <kelikcyckel@gmail.com>
2026-07-19 20:23:57 +0700

    fix: template overlay zIndex in brickType to render above grid cells

--- app/components/DrawingCanvas.tsx
+++ app/components/DrawingCanvas.tsx
@@ -476,7 +476,7 @@ export default function DrawingCanvas({
                     width: "100%",
                     height: "100%",
                     pointerEvents: "none",
-                    zIndex: 1,
+                    zIndex: 8,
                   }}
                 >
                   {referenceImage ? (
